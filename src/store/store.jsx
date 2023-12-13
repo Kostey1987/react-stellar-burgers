b942f2f8df430178e2f18c91e7a48aff210f70d6
Konstantin Samokhin <kostey87@gmail.com>
2023-12-13 15:24:59 +0300

    работа конструткор

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -3,6 +3,7 @@ import { api } from "../utils/api";
 import modalReducer from "../services/redusers/modal-slice";
 import itemReducer from "../services/redusers/ingredient-slice";
 import selectedReducer from "../services/redusers/current-slice";
+import constructorReduser from "../services/redusers/constructor-slice";
 
 export const store = configureStore({
   reducer: {
