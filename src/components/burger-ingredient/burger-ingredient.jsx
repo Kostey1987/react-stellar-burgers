d2398e3bc782aa1442446e6bf592956df2819601
Konstantin Samokhin <kostey87@gmail.com>
2023-12-17 01:42:54 +0300

    восстановлен счётчик

--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -7,24 +7,34 @@ import {
 import PropTypes from "prop-types";
 import { ingredientPropType } from "../../utils/prop-types";
 import { memo } from "react";
+import { useSelector } from "react-redux";
 
 const BurgerIngredient = memo(function BurgerIngredient({
   item,
   handleClickIngredient,
   onDrag,
 }) {
-  const onDragStart = () => {
-    onDrag(item);
-  };
+  // const onDragStart = () => {
+  //   onDrag(item);
+  // };
+
+  const { ingredients, bun } = useSelector((state) => state.constructor_slice);
+  const count = React.useMemo(() => {
+    if (item.type === "bun") {
+      return !!bun && bun._id === item._id ? 1 : 0;
+    } else {
+      return ingredients.filter((el) => el._id === item._id).length;
+    }
+  }, [ingredients, bun]);
   return (
     <div
       className={styles.card}
       onClick={() => handleClickIngredient(item)}
-      draggable
-      onDragStart={onDragStart}
+      // draggable
+      // onDragStart={onDragStart}
     >
       <div className={styles.counter}>
-        <Counter count={1} size="default" />
+        <Counter count={count} size="default" />
       </div>
       <img className={styles.image} src={item.image} alt={item.name} />
       <div className={styles.price}>
