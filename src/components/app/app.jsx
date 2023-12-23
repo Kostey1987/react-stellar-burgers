eedd0f0844a8ae54487ba03f5a91d55b8cdecc63
Konstantin Samokhin <kostey87@gmail.com>
2023-12-23 03:12:42 +0300

    init sprint14

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,81 +1,16 @@
 import React, { useCallback, useState } from "react";
-import styles from "./app.module.css";
-import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import Order from "../order/order";
-import Modal from "../modal/modal";
-import OrderDetails from "../order-details/order-details";
-import IngredientDetails from "../ingredient-details/ingredient-details";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleModal } from "../../services/redusers/modal-slice";
-import { fetchIngredients } from "../../services/ingredientsQuery";
-import {
-  selectIngredient,
-  clearSelectedIngredient,
-} from "../../services/redusers/current-slice";
-import { selectedIngredientSelector } from "../../services/selectors/modalSelectors";
-import { clearConstructor } from "../../services/redusers/constructor-slice";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../../pages/home/home";
 
 function App() {
-  const dispatch = useDispatch();
-  const currentIngredient = useSelector(selectedIngredientSelector);
-
-  const ingredients = useSelector((state) => state.items.itemsArray);
-  const currentOrder = useSelector((state) => state.sandwich.order);
-
-  React.useEffect(() => {
-    dispatch(fetchIngredients());
-  }, []);
-
-  const closeModal = useCallback(() => dispatch(toggleModal()), []);
-
-  const handleIngredientClick = React.useCallback(
-    (item) => {
-      dispatch(selectIngredient(item));
-    },
-    [dispatch]
-  );
-
-  const handleCloseIngredientModal = () => {
-    dispatch(clearSelectedIngredient());
-  };
-  const handleCloseCurrentOrderModal = () => {
-    dispatch(clearConstructor());
-  };
-
   return (
-    <div className={styles.app}>
-      <AppHeader />
-      <main className={styles.main}>
-        <section
-          className={styles.ingredients}
-          aria-label={`Ингредиенты бутерброда`}
-        >
-          <h1 className={`text text_type_main-large mt-10 mb-5`}>
-            Соберите бургер
-          </h1>
-          <BurgerIngredients handleIngredientClick={handleIngredientClick} />
-        </section>
-        <section
-          className={styles.ingredients}
-          aria-label={`Компоненты бутерброда`}
-        >
-          <BurgerConstructor />
-          <Order />
-        </section>
-      </main>
-      {!!currentIngredient && (
-        <Modal onClose={handleCloseIngredientModal}>
-          <IngredientDetails item={currentIngredient} />
-        </Modal>
-      )}
-      {currentOrder && (
-        <Modal onClose={handleCloseCurrentOrderModal}>
-          <OrderDetails />
-        </Modal>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Home />}></Route>
+        <Route path={"/test1"} element={<Home />}></Route>
+        <Route path={"/test2"} element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,80 @@
+import React, { useCallback, useState } from "react";
+import styles from "./home.module.css";
+import AppHeader from "../../components/app-header/app-header";
+import BurgerIngredients from "../../components/burger-ingredients/burger-ingredients";
+import BurgerConstructor from "../../components/burger-constructor/burger-constructor";
+import Order from "../../components/order/order";
+import Modal from "../../components/modal/modal";
+import OrderDetails from "../../components/order-details/order-details";
+import IngredientDetails from "../../components/ingredient-details/ingredient-details";
+import { useDispatch, useSelector } from "react-redux";
+// import { toggleModal } from "../../services/redusers/modal-slice";
+import { fetchIngredients } from "../../services/ingredientsQuery";
+import {
+  selectIngredient,
+  clearSelectedIngredient,
+} from "../../services/redusers/current-slice";
+import { selectedIngredientSelector } from "../../services/selectors/modalSelectors";
+import { clearConstructor } from "../../services/redusers/constructor-slice";
+
+function Home() {
+  const dispatch = useDispatch();
+  const currentIngredient = useSelector(selectedIngredientSelector);
+
+  const ingredients = useSelector((state) => state.items.itemsArray);
+  const currentOrder = useSelector((state) => state.sandwich.order);
+
+  React.useEffect(() => {
+    dispatch(fetchIngredients());
+  }, []);
+
+  const handleIngredientClick = React.useCallback(
+    (item) => {
+      dispatch(selectIngredient(item));
+    },
+    [dispatch]
+  );
+
+  const handleCloseIngredientModal = () => {
+    dispatch(clearSelectedIngredient());
+  };
+  const handleCloseCurrentOrderModal = () => {
+    dispatch(clearConstructor());
+  };
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <main className={styles.main}>
+        <section
+          className={styles.ingredients}
+          aria-label={`Ингредиенты бутерброда`}
+        >
+          <h1 className={`text text_type_main-large mt-10 mb-5`}>
+            Соберите бургер
+          </h1>
+          <BurgerIngredients handleIngredientClick={handleIngredientClick} />
+        </section>
+        <section
+          className={styles.ingredients}
+          aria-label={`Компоненты бутерброда`}
+        >
+          <BurgerConstructor />
+          <Order />
+        </section>
+      </main>
+      {!!currentIngredient && (
+        <Modal onClose={handleCloseIngredientModal}>
+          <IngredientDetails item={currentIngredient} />
+        </Modal>
+      )}
+      {currentOrder && (
+        <Modal onClose={handleCloseCurrentOrderModal}>
+          <OrderDetails />
+        </Modal>
+      )}
+    </div>
+  );
+}
+
+export default Home;
