03ff417251f9b8e7046f31770a8203f91ade94d8
Konstantin Samokhin <kostey87@gmail.com>
2024-02-02 19:50:05 +0300

    правки ревью спринт17

--- cypress/e2e/burder-constructor.cy.js
+++ cypress/e2e/burder-constructor.cy.js
@@ -1,3 +1,10 @@
+import {
+  testUrl,
+  testCardSelector,
+  testConructorSelector,
+  testButtonSelector,
+} from "../../src/utils/constants";
+
 describe("тестирование веб приложения", () => {
   it("загружаем массив ингредиентов", () => {
     cy.intercept("api/ingredients", {
@@ -6,76 +13,76 @@ describe("тестирование веб приложения", () => {
   });
 
   it("доступность сервиса по адрeсу localhost:3000", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit(testUrl);
   });
 
   it("открытие и закрытие попапа детали ингредиента", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit(testUrl);
     cy.wait(1000);
-    cy.get('[class^="burger-ingredient_card"]').first().click();
+    cy.get(testCardSelector).first().click();
     cy.contains("Детали ингредиента");
     cy.wait(1000);
     cy.get('[class^="modal_closeButton"]').click();
-    cy.visit("http://localhost:3000/");
+    cy.visit(testUrl);
   });
 
   it("тестируем основной функционал", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit(testUrl);
 
     cy.log("добавим булочки и ингредиенты");
-    cy.get('[class^="burger-ingredient_card"]')
+    cy.get(testCardSelector)
       .first()
       .trigger("dragstart")
       .wait(500)
-      .get('[class^="burger-constructor_components"]')
+      .get(testConructorSelector)
       .eq(1)
       .trigger("drop");
-    cy.get('[class^="burger-ingredient_card"]')
+    cy.get(testCardSelector)
       .eq(7)
       .trigger("dragstart")
       .wait(500)
-      .get('[class^="burger-constructor_components"]')
+      .get(testConructorSelector)
       .eq(1)
       .trigger("drop");
-    cy.get('[class^="burger-ingredient_card"]')
+    cy.get(testCardSelector)
       .eq(5)
       .trigger("dragstart")
       .wait(500)
-      .get('[class^="burger-constructor_components"]')
+      .get(testConructorSelector)
       .eq(1)
       .trigger("drop");
-    cy.get('[class^="burger-ingredient_card"]')
+    cy.get(testCardSelector)
       .eq(3)
       .trigger("dragstart")
       .wait(500)
-      .get('[class^="burger-constructor_components"]')
+      .get(testConructorSelector)
       .eq(1)
       .trigger("drop");
-    cy.get('[class^="burger-ingredient_card"]')
+    cy.get(testCardSelector)
       .eq(8)
       .trigger("dragstart")
       .wait(500)
-      .get('[class^="burger-constructor_components"]')
+      .get(testConructorSelector)
       .eq(1)
       .trigger("drop");
 
     cy.log("удалим лишние ингредиенты");
     cy.get("[class^=constructor-element__action]").eq(2).click();
-    cy.get('[class^="burger-constructor_components"]').eq(4).and("not.exist");
+    cy.get(testConructorSelector).eq(4).and("not.exist");
     // Нажимаем на кнопку офрмить заказ и редиректимся на страницу логина
     cy.log("нажимаем кнопку офрмления заказа");
-    cy.get('[class^="button"]').contains("Оформить заказ").click();
+    cy.get(testButtonSelector).contains("Оформить заказ").click();
     // Логинимся
     cy.log("ввод данных для входа");
     cy.get("input").first().type("test@test.com");
     cy.get("input").last().type("12345");
-    cy.get('[class^="button"]')
+    cy.get(testButtonSelector)
       .contains("Войти")
       .click()
       .intercept("api/auth/login", { fixture: "hardCodedlogin.json" });
     // Повторное оформление заказа
     cy.log("Повторная попытка оформления заказа");
-    cy.get('[class^="button"]')
+    cy.get(testButtonSelector)
       .contains("Оформить заказ")
       .click()
       .intercept(
